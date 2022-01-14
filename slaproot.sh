2e0f49268d6af42d6177b364dea47d7bf9ede192
ponas <47662158+ForceGROUP@users.noreply.github.com>
2022-01-14 14:52:13 +0200

    Add files via upload

---
+++ slaproot.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+clear
+# By @TuzasForce
+# root slaptažodzio keitimas
+[[ $(grep -c "prohibit-password" /etc/ssh/sshd_config) != '0' ]] && {
+	sed -i "s/prohibit-password/yes/g" /etc/ssh/sshd_config
+} > /dev/null
+[[ $(grep -c "without-password" /etc/ssh/sshd_config) != '0' ]] && {
+	sed -i "s/without-password/yes/g" /etc/ssh/sshd_config
+} > /dev/null
+[[ $(grep -c "#PermitRootLogin" /etc/ssh/sshd_config) != '0' ]] && {
+	sed -i "s/#PermitRootLogin/PermitRootLogin/g" /etc/ssh/sshd_config
+} > /dev/null
+[[ $(grep -c "PasswordAuthentication" /etc/ssh/sshd_config) = '0' ]] && {
+	echo 'PasswordAuthentication yes' > /etc/ssh/sshd_config
+} > /dev/null
+[[ $(grep -c "PasswordAuthentication no" /etc/ssh/sshd_config) != '0' ]] && {
+	sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
+} > /dev/null
+[[ $(grep -c "#PasswordAuthentication no" /etc/ssh/sshd_config) != '0' ]] && {
+	sed -i "s/#PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
+} > /dev/null
+service ssh restart > /dev/null
+clear
+echo -e "\033[1;32m DABAR GALITE IVESTI NAUJA root SALTAŽODI\033[0m"
+echo -e "\033[1;32m IVESKITE DU KARTUS TA PATI SLAPTAŽODI\033[0m"
+sleep 2s
+passwd
